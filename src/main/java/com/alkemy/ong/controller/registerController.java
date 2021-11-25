d74b8062f177c646f7222eaa96e2526ec0b8a49f
Cristian Silvestro <silvestro.cristian@gmail.com>
2021-11-25 20:46:40 -0300

    Branch rename: from CrisSilvestro to Jira ticket

--- src/main/java/com/alkemy/ong/controller/registerController.java
+++ src/main/java/com/alkemy/ong/controller/registerController.java
@@ -1,2 +1,19 @@
-package com.alkemy.ong.controller;public class registerController {
+package com.alkemy.ong.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequestMapping
+public class registerController {
+
+    /*Metodo que recibe parametros de registro: nombre, apellido, mail, clave y repetir clave.
+    Se marcan los atributos con RequestParam indicando que van a ser parametros de solicitud
+    y este controlador los va a recibir*/
+
+    @PostMapping("/register")
+    public String register (@RequestParam String firstName, @RequestParam String lastName, @RequestParam String email,
+                            @RequestParam String passwordA, @RequestParam String passwordB){
+        return "register.html";
+    }
+
 }
